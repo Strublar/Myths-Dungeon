f552b54142090dd0157beb6e7ec067df574782bb
Strublar <daluzmatteo@gmail.com>
2025-06-23 21:09:12 +0200

    Tooltips

--- Assets/Scripts/Tools/HeroTooltipManager.cs
+++ Assets/Scripts/Tools/HeroTooltipManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Misc;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,14 +12,20 @@ public class HeroTooltipManager : MonoBehaviour
     public GameObject heroToolTip;
     [Header("HeroTooltip")] public TMP_Text heroName;
     public Image heroImage;
-    public TMP_Text stats;
-    public TMP_Text abilityName;
+    //Stats
+    public TMP_Text attack;
+    public TMP_Text attackCooldown;
+    public TMP_Text health;
+    public TMP_Text armor;
+    public TMP_Text critChance;
+    public TMP_Text mastery
+        ;
     public TMP_Text ability;
     public TMP_Text passive;
-    public Image itemImage;
+    /*public Image itemImage;
     public TMP_Text itemDescription;
     public Image rarityFrame;
-    public Image background;
+    public Image background;*/
 
     public void Awake()
     {
@@ -30,35 +37,37 @@ public class HeroTooltipManager : MonoBehaviour
         var context = new Context
         {
             source = hero,
+            passiveHolder = hero
         };
         
         heroName.text = hero.definition.heroName;
         heroImage.sprite = hero.definition.heroImage;
-        /*stats.text = "\nHp :\t" + hero.maxHp +
-                     "\nArmor :\t" + hero.armor +
-                     "\nCrit% :\t" + hero.critChance + "%" +
-                     "\nCritPow :\t" + hero.critPower + "%" +
-                     "\nThreat :\tx" + hero.definition.threatRatio;
-
-        abilityName.text = hero.ability.abilityName + " (" + $"{hero.ability.cooldown / hero.abilityHaste * 100:0.#}" + "s)";
-
+        attack.text = "<b><color=#CC6600>Attack\n\n" + hero.GetCarac(Carac.attack) + "</color></b>";
+        attackCooldown.text = "<b><color=#008B8B>At.CD\n\n" + hero.GetCarac(Carac.attack) + "</color></b>";
+        health.text = "<b><color=#006600>Health\n\n" + hero.GetCarac(Carac.attack) + "</color></b>";
+        armor.text = "<b><color=#333333>Armor\n\n" + hero.GetCarac(Carac.attack) + "</color></b>";
+        critChance.text = "<b><color=#990000>Crit%\n\n" + hero.GetCarac(Carac.attack) + "</color></b>";
+        mastery.text = "<b><color=#660066>Mast.\n\n" + hero.GetCarac(Carac.attack) + "</color></b>";
+        
         var formatListAbility = new string[hero.ability.linkedPassives[0].values.Count];
         for (int i = 0; i < hero.ability.linkedPassives[0].values.Count; i++)
         {
             formatListAbility[i] = hero.ability.linkedPassives[0].values[i].computeValue(context).ToString();
         }
 
-        ability.text = string.Format(hero.ability.linkedPassives[0].description, formatListAbility);
+        ability.text = "Ability :\n\n"+string.Format(hero.ability.linkedPassives[0].description, formatListAbility);
 
-        string[] formatListSkill = new string[hero.skill.values.Count];
-        for (int i = 0; i < hero.skill.values.Count; i++)
+        var passiveDefinition = hero.definition.passives[0];
+
+        string[] formatListPassive = new string[passiveDefinition.values.Count];
+        for (int i = 0; i < hero.definition.passives[0].values.Count; i++)
         {
-            formatListSkill[i] = hero.skill.values[i].computeValue(context).ToString();
+            formatListPassive[i] = passiveDefinition.values[i].computeValue(context).ToString();
         }
 
-        passive.text = string.Format(hero.skill.description, formatListSkill);
+        passive.text = "Passive :\n\n"+string.Format(passiveDefinition.description, formatListPassive);
 
-        string[] formatListItem = Array.Empty<string>();
+        /*string[] formatListItem = Array.Empty<string>();
         if (hero.item.definition != null)
         {
             formatListItem = new string[hero.item.definition.values.Count];
