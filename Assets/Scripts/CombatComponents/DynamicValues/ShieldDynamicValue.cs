7f4849fa3a8786ab67e23a6343a27832921236a7
Strublar <daluzmatteo@gmail.com>
2025-06-26 07:51:22 +0200

    Shield slam

---
+++ Assets/Scripts/CombatComponents/DynamicValues/ShieldDynamicValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShieldValue", menuName = "DynamicValue/ShieldValue")]
+public class ShieldDynamicValue : DynamicValue
+{
+
+    public TargetSelector targetSelector;
+    public override int computeValue(Context context)
+    {
+        return targetSelector.GetTargets(context).Sum(t => t.ComputeShieldValue());;
+    }
+}
