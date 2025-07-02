2ce7e535db7dd1372479906ca644b48068503820
Strublar <daluzmatteo@gmail.com>
2025-07-02 08:22:15 +0200

    Icons

---
+++ Assets/Scripts/Tools/RenameSkillIcons.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class RenameSkillIcons
+{
+    [MenuItem("Tools/Rename Skill Icons")]
+    public static void RenameIcons()
+    {
+        string folderPath = "Assets/Resources/Icons/Skills"; 
+
+        string[] files = Directory.GetFiles(folderPath, "*.png");
+
+        foreach (var path in files)
+        {
+            string filename = Path.GetFileName(path);
+
+            if (!filename.StartsWith("Skill icon - "))
+            {
+                string newName = "Skill icon - " + filename;
+                string newPath = Path.Combine(folderPath, newName);
+
+                AssetDatabase.RenameAsset(path, newName);
+                Debug.Log($"Renamed {filename} -> {newName}");
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
