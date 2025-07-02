63232e5f4409dfcbba32ec59cb24f658064810d6
Strublar <daluzmatteo@gmail.com>
2025-07-02 18:36:49 +0200

    Fix constant scaling to work with all caracs

--- Assets/Scripts/CombatComponents/DynamicValues/ConstantScalingDynamicValue.cs
+++ Assets/Scripts/CombatComponents/DynamicValues/ConstantScalingDynamicValue.cs
@@ -1,4 +1,5 @@
-﻿using Misc;
+﻿using System;
+using Misc;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Value", menuName = "DynamicValue/ConstantScaling")]
@@ -10,6 +11,29 @@ public class ConstantScalingDynamicValue : DynamicValue
     public override int computeValue(Context context)
     {
         if (context.passiveHolder == null) return value;
+        if (context.passiveHolder is Hero hero)
+        {
+            var baseCaracValue = hero.definition.caracs.Find(c => c.carac == scalingCarac);
+
+            int offset = 0;
+            switch (scalingCarac)
+            {
+                case Carac.Armor:
+                    offset = 100;
+                    break;
+                case Carac.CritChance:
+                    offset = 100;
+                    break;
+                case Carac.Mastery:
+                    offset = 100;
+                    break;
+                default:
+                    offset = 0;
+                    break;
+            }
+
+            return value * (context.passiveHolder.GetCarac(scalingCarac) + offset) / (baseCaracValue.value+offset);
+        } 
         return value * context.passiveHolder.GetCarac(scalingCarac)/100;
     }
 }
