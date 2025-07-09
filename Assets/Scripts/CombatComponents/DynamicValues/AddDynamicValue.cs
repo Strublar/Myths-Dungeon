6c8eb9f342fc4416e31794df4f5accf30fccd673
Strublar <daluzmatteo@gmail.com>
2025-07-09 08:06:39 +0200

    Additional skill and fixs

---
+++ Assets/Scripts/CombatComponents/DynamicValues/AddDynamicValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AddValue", menuName = "DynamicValue/Operation/Add")]
+public class AddDynamicValue : DynamicValue
+{
+    public List<DynamicValue> values;
+    protected override int ComputeValue(Context context)
+    {
+        var finalValue = 0;
+        foreach (var dynamicValue in values)
+        {
+            finalValue += dynamicValue.Compute(context);
+        }
+
+        return finalValue;
+    }
+    
+}
